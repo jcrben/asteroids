0848f2d524488cda7f6916747e49f9ca9d44d34d
jcrben <contact@bencreasy.com>
2015-02-13 21:06:25 -0800

    Change images

--- watchout.js
+++ watchout.js
@@ -1,101 +1,99 @@
 // start slingin' some d3 here.
+var Game = function(){
+  this.score = 0;
+  this.highScore = 0;
+  this.collision = 0;
+  this.board = d3.select('.board');
+  this.enemies = undefined;
+  this.player = undefined;
 
-var createAsteroids = function(amount) {
+  this.setBoard(this.createAsteroids(10));
+  this.change();
+}
+
+
+Game.prototype.createAsteroids = function(amount) {
   var results = [];
   for(var i = 0; i < amount; i++){
-    results.push([(Math.floor(Math.random() * (650)))+25,
-                  (Math.floor(Math.random() * (650)))+25]
-                );
+    results.push([(Math.floor(Math.random() * (650)))+25, (Math.floor(Math.random() * (650)))+25]);
   }
-  // [[top, left], [top, left]]
   return results;
 };
 
-var score = 0;
-var collision = 0;
-var highScore = 0;
-var board = d3.select('.board');
-// d3.range(50)
-var setBoard = function(asteroids){
-var enemies = board.selectAll()
-      .data(asteroids)
-      .enter()
-      .append('svg:circle')
-      .attr('class', 'asteroid')
-      .attr('cx', function(d){
-        return d[1];
-      })
-      .attr('cy', function(d){
-        return d[0];
-      })
-      .attr('r', '25')
-      .attr('filter', 'url(#rock)');
-      // .attr();
+Game.prototype.setBoard = function(asteroids){
+  this.enemies = this.board.selectAll()
+                      .data(asteroids)
+                      .enter()
+                      .append('svg:circle')
+                      .attr({
+                              class: 'asteroid',
+                              cx : function(d){ return d[1]; },
+                              cy : function(d){ return d[0]; },
+                              r : 25,
+                              filter : 'url(#rock)'
+                            });
 
+  var context = this;
 
-var drag = d3.behavior.drag()
-             .on('dragstart', function(){
+  var drag = d3.behavior.drag()
+                .on('dragstart', function(){})
+                .on('drag', function(){
+                  var x = d3.event.x;
+                  var y = d3.event.y;
 
-             })
-             .on('drag', function(){
-                var x = d3.event.x;
-                var y = d3.event.y;
+                  if(x < 675 && x > 25){
+                    context.player.attr('cx', x)
+                  }
+                  if(y < 675 && y > 25){
+                    context.player.attr('cy', y)
+                  }
+                })
+             .on('dragend', function() {});
 
-                if(x < 675 && x > 25){
-                  circle.attr('cx', x)
-                }
-                if(y < 675 && y > 25){
-                  circle.attr('cy', y)
-                }
-             })
-             .on('dragend', function() {
-             });
+  this.player = this.board.selectAll()
+                     .data([[350, 350]])
+                     .enter()
+                     .append('svg:circle')
+                     .attr('class', 'player')
+                     .attr('cx', function(d){
+                       return d[1];
+                     })
+                     .attr('cy', function(d){
+                       return d[0];
+                     })
+                     .attr('r', '25')
+                     .attr('filter', 'url(#player)')
+                     .call(drag);
 
-  var circle = board.selectAll()
-       .data([[350, 350]])
-       .enter()
-       .append('svg:circle')
-       .attr('class', 'player')
-       .attr('cx', function(d){
-         return d[1];
-       })
-       .attr('cy', function(d){
-         return d[0];
-       })
-       .attr('r', '25')
-       .attr('fill', 'blue')
-       .call(drag);
 
   d3.timer(function(){
-    // enemies.each(function(){console.log(this)});
-
-
-    enemies.each(function () {
+    context.enemies.each(function () {
       var enemy = d3.select(this);
       var enemyX = enemy.attr('cx');
       var enemyY = enemy.attr('cy');
-      var x = circle[0][0].cx.animVal.value;
-      var y = circle[0][0].cy.animVal.value;
-      if(score > highScore){
-        highScore = score;
+      var x = context.player[0][0].cx.animVal.value;
+      var y = context.player[0][0].cy.animVal.value;
+      if(context.score > context.highScore){
+        context.highScore = context.score;
       }
       if(Math.abs(x - enemyX) < 50 && Math.abs(y - enemyY) < 50){
         console.log('hit');
-        collision++;
-        score = 0;
+        context.collision++;
+        context.score = 0;
       }
-    })
+    });
 
-    d3.select('#current').text(score);
-    d3.select('#collisions').text(collision);
-    d3.select('#high').text(highScore);
-    score++;
+    d3.select('#current').text(context.score);
+    d3.select('#collisions').text(context.collision);
+    d3.select('#high').text(context.highScore);
+    context.score++;
     return false;
   });
+
 }
 
-var transition = function(arr){
-  board.selectAll('.asteroid')
+Game.prototype.transition = function(arr){
+  this.board.selectAll('.asteroid')
     .data(arr)
     .transition()
     .duration(1000)
@@ -108,25 +106,16 @@ var transition = function(arr){
     .attr('r', '25')
     .attr('filter', 'url(#rock)');
 }
-var createPlayer = function() {
+Game.prototype.createPlayer = function() {
   return [[Math.floor((window.innerHeight-20)/2),
         Math.floor((window.innerWidth-20)/2)]];
 };
 
 
-var change = function(){
-  var asteroids = createAsteroids(10);
-  transition(asteroids);
-  setTimeout(change.bind(this), 1000);
+Game.prototype.change = function(){
+  var asteroids = this.createAsteroids(10);
+  this.transition(asteroids);
+  setTimeout(this.change.bind(this), 1000);
 };
 
-var checkCollision = function() {
-
-}
-setTimeout()
-setBoard(createAsteroids(10));
-change();
-
-// instiantiate nodes as svgs
-// settimer -- may ways to this
-// Create a player
+var game = new Game();
